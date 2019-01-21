50ca95d8267612c2c2881ae81a60fa3231ba8c8f
Aleksi Hiltunen <teemu.hiltunen@tut.fi>
2019-01-21 13:26:46 +0200

    Created data getters for nuorten ideat and lausuntopalvelut

---
+++ VC_data_getter.py
@@ -0,0 +1,66 @@
+import json
+import os
+import requests
+import sys
+
+TO_JSON_SUFFIX = "?format=json"
+MYDIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(MYDIR, "req", "xmltodict"))
+
+import xmltodict
+
+class VC_data_getter:
+	def __init__(self):
+		
+		self.urls = self.read_json_file(os.path.join(MYDIR, "common_urls.json"))
+		self.headers = {'Content-type': 'application/json'}
+	
+	def read_json_file(self, path):
+		try:
+			with open(path) as f:
+				raw = f.read()
+				data = json.loads(raw)
+				
+		except Exception as e:
+			print("Something went wrong (read_common_urls):", e)
+			
+		return data
+	
+	def get_nuortenideat_all(self):
+		url = os.path.join(	self.urls["nuortenideat"]["base"], 
+							self.urls["nuortenideat"]["ideas"])
+		url += TO_JSON_SUFFIX
+		ideas = []
+		for i in range(1,1000):
+			url += "&page=" + str(i)
+			r = requests.get(url, headers=self.headers)
+			data = r.json()
+			try:
+				_c = data["detail"]
+				break
+			except Exception as e:
+				ideas.extend(data["results"])
+				
+		return ideas
+		
+	def get_nuortenideat_by_id(self, id):
+		url = str(id)
+		url += TO_JSON_SUFFIX
+		return self.get_request_json(url)
+		
+	def get_lausuntopalvelu_all(self):
+		url = os.path.join(	self.urls["lausuntopalvelu"]["base"], 
+							self.urls["lausuntopalvelu"]["proposals"])
+		return_xml = self.get_request_text(url)
+		data = json.loads(json.dumps(xmltodict.parse(return_xml)))
+		return data["feed"]["entry"]
+		
+	def get_request_json(self, url):		
+		r = requests.get(url, headers=self.headers)
+		return r.json()["results"]
+		
+	def get_request_text(self, url):
+		r = requests.get(url)
+		return r.text
+		
+	
