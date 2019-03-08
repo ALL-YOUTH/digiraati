fcd48694e979affe5e0e0bf69a2443eabb2d18dc
Aleksi Hiltunen <teemu.hiltunen@tut.fi>
2019-03-08 15:01:38 +0200

    Added history navbar

--- digiraati/digiraati_server.js
+++ digiraati/digiraati_server.js
@@ -4,32 +4,41 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var host = "localhost";
 
+//Chatters are arrays of [socketID, name]
 var chatters = [];
+//Messages are arrays of [name, msg]
+var messages = [];
+var MESSAGES2PRINT = 10;
 
+//Chat page getter
 app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/chat.html');
 });
 
+//Home page getter
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/home.html');
 });
 
+//Connection
 io.on('connection', function(socket){
-  client = socket["client"]["id"];
-  if(chatters.indexOf(client) == -1){
+  client = socket.request.connection.remoteAddress;
+  if(get_name(client) == -1){
     //New user entered the channel
     console.log("New user");
     socket.emit('new user');
   }
   else{
-    socket.emit("Welcome back, " + get_name(client));
+    chatter = get_name(client);
+    socket.emit('chat message', "Welcome back, " + chatter);
+    print_msgs(socket, MESSAGES2PRINT);
   }
   socket.on('name submit', function(name){
     console.log("Client submitted a name", name);
     for(i = 0; i < chatters.length; ++i){
       //nickname used
       if(chatters[i][1].toLowerCase() == name.toLowerCase()){
-        console.log("Name already taken");
+        //console.log("Name already taken");
         socket.emit('invalid nickname');
         return;
       }
@@ -42,10 +51,11 @@ io.on('connection', function(socket){
 
   //SENDING A MESSAGE PART
   socket.on('chat message', function(msg){
-    client = socket["client"]["id"];
-    console.log("Message was sent \"", msg + "\"");
+    client = socket.request.connection.remoteAddress;
+    //console.log("Message was sent \"", msg + "\"");
     send_msg = get_name(client) + ": " + msg;
     io.emit('chat message', send_msg);
+    messages.push(send_msg);
   });
 });
 
@@ -61,4 +71,17 @@ function get_name(id){
     }
   }
   return -1;
+}
+
+function print_msgs(socket, num_to_print){
+  if(messages.length <= num_to_print){
+    for(i = 0; i < messages.length; ++i){
+      socket.emit('chat message', messages[i]);
+    }
+  }
+  else{
+    for(i = messages.length - num_to_print - 1; i < messages.length; ++i){
+      socket.emit('chat message', messages[i]);
+    }
+  }
 }
