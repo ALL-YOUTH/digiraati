3f6369a023084b1b9a91b8c99584cbc685c1bf83
Aleksi Hiltunen <teemu.hiltunen@tut.fi>
2019-10-18 11:20:07 +0300

    added council creation page

--- digiraati/js/create.js
+++ digiraati/js/create.js
@@ -5,3 +5,26 @@ $(function(){
   $('#header').load(host + "/html/header.html");
   $('#footer').load(host + "/html/footer.html");
 });
+
+$('textarea').on('keydown', function(e){
+
+}).on('input', function(){
+    $(this).height(1);
+    var totalHeight = $(this).prop('scrollHeight') - parseInt($(this).css('padding-top')) - parseInt($(this).css('padding-bottom'));
+    if(totalHeight < 30){
+      totalHeight = 30;
+    }
+    $(this).height(totalHeight);
+});
+
+$('#no_limit_radiobtn').change(function(){
+  if($(this).is(':checked')){
+    document.getElementById('limit_number').style.display = "none";
+  }
+});
+
+$('#limit_radiobtn').change(function(){
+  if($(this).is(':checked')){
+    document.getElementById('limit_number').style.display = "inline-block";
+  }
+});
