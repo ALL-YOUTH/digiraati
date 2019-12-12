a06f2b0eb5b47e412f3cec6085589852e12608ec
Aleksi Hiltunen <teemu.hiltunen@tut.fi>
2019-12-12 15:20:26 +0200

    removed lobby description from chatroom

--- digiraati/js/lobby_chat.js
+++ digiraati/js/lobby_chat.js
@@ -26,8 +26,7 @@ $(function(){
 socket.on('council data', function(data){
   $('#left_menu_title').html(data["name"].toUpperCase());
   $('#chat_title').html(data["name"].toUpperCase());
-  //$('#chat_timetable').html("Käynnissä:", data["startdate"], data["starttime"], " - ", data["enddate"], data["endtime"]);
-  $('#chat_description').html(data["description"]);
+  //$('#chat_description').html(data["description"]);
   $('#chat_hashtag').html("#"+data["name"].toLowerCase().replace(" ", ""));
   var h = document.getElementById('chat_content').offsetHeight +
           document.getElementById('header').offsetHeight;
