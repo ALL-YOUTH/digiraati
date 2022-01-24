7d7b7efa20a09dcbca9e566ad840e04ecad53a66
Petri Louhenheimo <1541962+louhenheimo@users.noreply.github.com>
2022-01-24 19:17:29 +0200

    Added collapsibles at Info page

--- digiraati/js/info.js
+++ digiraati/js/info.js
@@ -3,5 +3,21 @@ var host = socket["io"]["uri"] + ":" + location.port;
 
 $(function(){
   $('#header').load(socket["io"]["uri"] + "/html/header.html");
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
   $('#footer').load(socket["io"]["uri"] + "/html/footer.html");
 });
+
+
